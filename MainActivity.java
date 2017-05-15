76a50f1d76a18eacf7c91f8d9405b3555a20bfcb
YhisLara <yhlara@alumnos.ubiobio.cl>
2017-05-15 10:49:43 -0400

    save references

---
+++ MainActivity.java
@@ -0,0 +1,22 @@
+private SharedPreferences spref;
+
+private EditText email;
+private EditText pass;
+private Switch remember;
+private Button btnLogin;
+
+onCreate {
+
+  BindUI();
+
+
+}
+
+
+private void BindUI(){
+// referencias
+email = (EditText) findViewById(R.id.);
+pass = (EditText) findViewById(R.id).;
+rememeber = (Switch) findViewById(R.id.);
+btnLOgin = (Button) findViewById(R.id.);
+}
