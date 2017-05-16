6f4f62e77a7b1bdcaa769fd86336b24109b97334
YhisLara <yhlara@alumnos.ubiobio.cl>
2017-05-16 10:03:16 -0400

    update, new functions to return user values saved

--- SplashActivity.java
+++ SplashActivity.java
@@ -11,7 +11,19 @@ spref = getSharedPreferences("preferences",Context.MODE_PRIVATE);
 Intent intentLogin = new Intent(this, LoggyActivity.class);
 Intent intentMain = new Intent(this, MainActivity.class);
 
-if(!Utils.getUserMailPref().isEmpty) && !Utils.getUserPassPref.isEmpty())
-startActivity(intent);
+  if(!Utils.getUserMailPref().isEmpty && !Utils.getUserPassPref.isEmpty()){
+    startActivity(intentMain);
+  }
+  else {
+    startActivity(intentLogin);
+  }
+  finishi();
+}
 
+private void getUserMailPref(){
+  return spref.getString("email","");
+}
+
+private void getUserPassPref(){
+  return spref.getString("pass","");
 }
