86e42ffb66d1630314367ab8deff0d6d672b45c8
YhisLara <yhlara@alumnos.ubiobio.cl>
2017-05-16 10:25:00 -0400

    update, add new function
    added two new functions to return user preferences

--- utils.java
+++ utils.java
@@ -1,1 +1,7 @@
-private SharedPreferences spref;
+public class Utils {
+  
+ public static getUserMailPref(SharedPreferences preferences){return preferences.getString("email","");}
+ 
+ public static getUserPassPref(SharedPreferences preferences){return preferences.getString("pass","");}
+  
+}
