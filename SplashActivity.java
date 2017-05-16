2f79663e4288bb351e0f33c14102bda74be29116
YhisLara <yhlara@alumnos.ubiobio.cl>
2017-05-16 10:27:44 -0400

    update, user preferences
    deleted two functions to get user preferences, replaced for call to utils class

--- SplashActivity.java
+++ SplashActivity.java
@@ -11,19 +11,11 @@ spref = getSharedPreferences("preferences",Context.MODE_PRIVATE);
 Intent intentLogin = new Intent(this, LoggyActivity.class);
 Intent intentMain = new Intent(this, MainActivity.class);
 
-  if(!Utils.getUserMailPref().isEmpty && !Utils.getUserPassPref.isEmpty()){
+  if(!TextUtils.isEmpty(Utils.getUserMailPref(spref)) && !TextUtils.isEmpty(Utils.getUserPassPref(spref)){
     startActivity(intentMain);
   }
   else {
     startActivity(intentLogin);
   }
   finishi();
-}
-
-private void getUserMailPref(){
-  return spref.getString("email","");
-}
-
-private void getUserPassPref(){
-  return spref.getString("pass","");
 }
