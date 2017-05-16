94ab1fb6c47214c6a8d37c80bda7f3a116a6ffd5
YhisLara <yhlara@alumnos.ubiobio.cl>
2017-05-16 10:23:35 -0400

    update set flags,  utils preferences
    update to set flags and change in function to get user preferences

--- LoggyActivity.java
+++ LoggyActivity.java
@@ -36,15 +36,22 @@ btnLog = (Button) findViewById(R.id.btnLog);
 private void Verificar(){
 
 if(mail.lenght <= 0){
- Toast.makeText(this,"this fields mail can't be empty,please try again",Toast.LENGHT_SHORT).show();}
+ Toast.makeText(this,"this fields mail can't be empty,please try again",Toast.LENGHT_SHORT).show();
+  return false;
+}
  elseif(pass.lenght <= 0){
- Toast.makeText(this,"this fields pass can't be empty, please try again", Toast.LENGHT_SHORT).show();}
+ Toast.makeText(this,"this fields pass can't be empty, please try again", Toast.LENGHT_SHORT).show();
+   return false;
+ }
  else{
+   return true;
   }
 }
 
 private void goToMain(){
  Intent main = new Intent(this,MainActivity.class);
+ //flags para cerrar activity y no regresar a la anterior
+ main.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
  starActivity(main);
 }
 
@@ -53,14 +60,14 @@ private void SavePreferences(email,password,remember){
     SharedPreferences.Editor edtor = spref.edit();
     editor.putString("email",email);
     editor.putString("pass",pass);
-    editor.putBoolean("swith",remember);
     editor.commit();
+    editor.apply();
   }
 }
 
 private void setCredentialIfExist(){
-  String email = spref.getString("email");
-  String password = spref.getString("pass");
+  String email = Utils.getUserMailPref(spref);
+  String password = Utils.getUserPassPref(spref);
  
   if(email.lenght > 0 && pass.lenght > 0){
     mail.setText(email);
