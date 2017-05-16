b85713763cee390d828b6ed5657aa3036cbade58
YhisLara <yhlara@alumnos.ubiobio.cl>
2017-05-16 12:46:25 -0400

    update main
    new function to menu, delete shared preferences and log out.

--- MainActivity.java
+++ MainActivity.java
@@ -20,3 +20,27 @@ pass = (EditText) findViewById(R.id.editTextPass);
 rememeber = (Switch) findViewById(R.id.SRemember);
 btnLOgin = (Button) findViewById(R.id.btnLog);
 }
+
+private void logOut(){
+  Intent logout = new Intent(this,LoggyActivity.class);
+  logout.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TAKS);
+  startActivity(logout);
+}
+public boolean onCreateOptionsMenu(Menu menu){
+  getInflater().inflate(R.menu.menu_main, menu);
+  return true;
+}
+
+public boolean onOptionsItemSelected(MenuItem item){
+  switch (item.getItemId()){
+    case R.id.item1:
+      logOut();
+      return true;
+    case R.id.item2:
+      logOut();
+      Utils.deleteUserAndPass(spref);
+      return true;
+    default:
+      return super.onOptionsItemSelected(item);  
+  }
+}
